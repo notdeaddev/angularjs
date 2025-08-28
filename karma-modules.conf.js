926a621435c48862b5d2433e818f872b58459b32
Bertrand Martin <32521698+bertysentry@users.noreply.github.com>
2025-08-28 13:10:36 +0200

    test(modules): run module specs

--- karma-modules.conf.js
+++ karma-modules.conf.js
@@ -1,12 +1,23 @@
 'use strict';
 
+var angularFiles = require('./angularFiles');
 var sharedConfig = require('./karma-shared.conf');
 
 module.exports = function(config) {
   sharedConfig(config, {testName: 'AngularJS: isolated module tests', logFile: 'karma-modules-isolated.log'});
 
+  var moduleTests = angularFiles.mergeFilesFor(
+    'karmaModules-ngAria',
+    'karmaModules-ngCookies',
+    'karmaModules-ngMessageFormat',
+    'karmaModules-ngMessages',
+    'karmaModules-ngResource',
+    'karmaModules-ngRoute',
+    'karmaModules-ngSanitize',
+    'karmaModules-ngTouch'
+  );
+
   config.set({
-    failOnEmptyTestSuite: false,
     files: [
       'build/angular.js',
       'build/angular-mocks.js',
@@ -14,8 +25,7 @@ module.exports = function(config) {
       'test/helpers/matchers.js',
       'test/helpers/privateMocks.js',
       'test/helpers/support.js',
-      'test/helpers/testabilityPatch.js',
-      'build/test-bundles/angular-*.js'
-    ]
+      'test/helpers/testabilityPatch.js'
+    ].concat(moduleTests)
   });
 };
