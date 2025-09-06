30de89268a4666577673d815a164c2f8df04a083
Bertrand Martin <32521698+bertysentry@users.noreply.github.com>
2025-09-06 12:07:30 +0200

    test(e2e): stabilize docs examples and enforce workflow failure

--- test/e2e/server.js
+++ test/e2e/server.js
@@ -22,6 +22,17 @@ module.exports = function createServer() {
 
       staticServer(req, res, function(staticErr) {
         if (staticErr) {
+          if (staticErr.status === 404 && /^\/build\/docs\//.test(req.url) && !path.extname(req.url)) {
+            req.url = '/build/docs/index-test.html';
+            staticServer(req, res, function(err) {
+              if (err) {
+                res.statusCode = err.status || 500;
+                res.end(err.message);
+              }
+            });
+            return;
+          }
+
           res.statusCode = staticErr.status || 500;
           res.end(staticErr.message);
           return;
